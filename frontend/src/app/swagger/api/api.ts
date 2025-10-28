2bc0036c762249ac8a9178e16a9d953184ce451d
LukasGrabher2006 <145109786+LukasGrabher2006@users.noreply.github.com>
2025-10-28 16:19:39 +0100

    updated Backend CORS config in appSettings

--- frontend/src/app/swagger/api/api.ts
+++ frontend/src/app/swagger/api/api.ts
@@ -1,3 +1,9 @@
-export * from './authentication.service';
-import { AuthenticationService } from './authentication.service';
-export const APIS = [AuthenticationService];
+export * from './category.service';
+import { CategoryService } from './category.service';
+export * from './categoryValue.service';
+import { CategoryValueService } from './categoryValue.service';
+export * from './column.service';
+import { ColumnService } from './column.service';
+export * from './layout.service';
+import { LayoutService } from './layout.service';
+export const APIS = [CategoryService, CategoryValueService, ColumnService, LayoutService];
